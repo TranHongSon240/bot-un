9903bd05e2ea8bac88daa1d2facd07c617f992b8
TranHongSon240 <147250868+TranHongSon240@users.noreply.github.com>
2026-05-02 18:53:33 +0700

    Update un_bot_v2.py

--- un_bot_v2.py
+++ un_bot_v2.py
@@ -24,8 +24,7 @@ MAX_PERMANENT_MEMBERS = 5
 mongo_client = motor.motor_asyncio.AsyncIOMotorClient(
     MONGO_URL,
     serverSelectionTimeoutMS=5000,
-    tls=True,
-    tlsAllowInvalidCertificates=True
+    tlsInsecure=True
 )
 db            = mongo_client["WorldRP_2000"]
 resolutions_col = db["UN_Resolutions"]
